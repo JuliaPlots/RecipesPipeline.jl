48fb0ade91d0ce8a633f8165d1c012a8a97d7c74
Jerry Ling <proton@jling.dev>
2020-10-22 08:57:15 -0400

    Revert "AbstractDict plot default to sorted (st=:line)"

--- src/user_recipe.jl
+++ src/user_recipe.jl
@@ -230,10 +230,8 @@ end
 end
 
 # Dicts: each entry is a data point (x,y)=(key,value)
-@recipe function f(d::AbstractDict)
-    seriestype --> :line
-    collect(keys(d)), collect(values(d))
-end
+@recipe f(d::AbstractDict) = collect(keys(d)), collect(values(d))
+
 # function without range... use the current range of the x-axis
 @recipe function f(f::FuncOrFuncs{F}) where {F <: Function}
     plt = plotattributes[:plot_object]
