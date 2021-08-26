4da94a7ae088e71c3000eac7ab5063642ed8f91c
Nicholas Bauer <nicholasbauer@outlook.com>
2021-08-25 21:11:45 -0400

    Compatibility

--- src/plot_recipe.jl
+++ src/plot_recipe.jl
@@ -27,20 +27,29 @@ function _process_plotrecipe(plt, kw, kw_list, still_to_process)
         push!(kw_list, kw)
         return
     end
-    st = kw[:seriestype]
-    st = kw[:seriestype] = type_alias(plt, st)
-    datalist = RecipesBase.apply_recipe(kw, Val{st}, plt)
-    if !isnothing(datalist)
-        warn_on_recipe_aliases!(plt, datalist, :plot, st)
-        for data in datalist
-            preprocess_attributes!(plt, data.plotattributes)
-            if data.plotattributes[:seriestype] == st
-                error("Plot recipe $st returned the same seriestype: $(data.plotattributes)")
+    try
+        st = kw[:seriestype]
+        st = kw[:seriestype] = type_alias(plt, st)
+        datalist = RecipesBase.apply_recipe(kw, Val{st}, plt)
+        if isnothing(datalist)
+            warn_on_recipe_aliases!(plt, datalist, :plot, st)
+            for data in datalist
+                preprocess_attributes!(plt, data.plotattributes)
+                if data.plotattributes[:seriestype] == st
+                    error("Plot recipe $st returned the same seriestype: $(data.plotattributes)")
+                end
+                push!(still_to_process, data.plotattributes)
             end
-            push!(still_to_process, data.plotattributes)
+        else
+            push!(kw_list, kw)
         end
-    else
-        push!(kw_list, kw)
+    catch err
+        # the try/catch block can be removed when Plots.jl makes the compensating change
+        if err isa MethodError && occursin("plot recipe", err.args)
+            push!(kw_list, kw)
+        else
+            rethrow()
+        end
     end
     return
 end
